383e9f175aadfdbd57f3afaea1ba789d39af7fd8
Alex Deidda <alex.deidda@aledesign.co.uk>
2023-11-03 14:09:07 +0000

    Diagram removed

--- style.py
+++ style.py
@@ -1,5 +1,3 @@
-# Filename: style.py
-
 __version__ = '0.3'
 __author__ = 'Alessio Deidda / Cecilia Baggini'
 
@@ -7,10 +5,13 @@ mainWindow = 'QMainWindow { background: #000000 url("imgs/main_bg.gif") center l
 cameraWindow = 'QWidget { background: #000000; }'
 playerWindow = 'QWidget { background: #ffffff; } '
 
-btn_quit = 'QPushButton { height: 256px; width: 128px; border: none; background: url("imgs/quit-icon.png") center no-repeat; }'
-btn_navit = 'QPushButton { height: 256px; width: 128px; border: none; background: url("imgs/navit-icon.png") center no-repeat; }'
-btn_camera = 'QPushButton { height: 256px; width: 128px; border: none; background: url("imgs/camera-icon.png") center no-repeat; }'
-btn_sensors = 'QPushButton { height: 256px; width: 128px; border: none; background: url("imgs/sensors-icon.png") center no-repeat; }'
-btn_back = 'QPushButton { height: 128px; width: 256px; border: none; background: url("imgs/back-icon.png") center no-repeat; }'
+btn_H = 'QPushButton { height: 256px; width: 128px; border: none;'
+btn_V = 'QPushButton { height: 128px; width: 256px; border: none;'
+
+btn_quit = btn_H + ' background: url("imgs/quit-icon.png") center no-repeat; }'
+btn_navit = btn_H + ' background: url("imgs/navit-icon.png") center no-repeat; }'
+btn_camera = btn_H + ' background: url("imgs/camera-icon.png") center no-repeat; }'
+btn_sensors = btn_H + ' background: url("imgs/sensors-icon.png") center no-repeat; }'
+btn_back = btn_V + ' background: url("imgs/back-icon.png") center no-repeat; }'
 
 ### NOTES: set a common style when required, maybe a string variable that can be attached
